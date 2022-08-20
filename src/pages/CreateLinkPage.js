16ba8065d7e7263eac8794be6a6a1010e79c894f
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-08-20 16:33:34 +0200

    sc

--- src/pages/CreateLinkPage.js
+++ src/pages/CreateLinkPage.js
@@ -1,10 +1,83 @@
 import React from 'react'
 import DashboardLayout from "./../layout/DashboardLayout";
+import { Button, Form, Input, notification } from 'antd';
+import {collection, addDoc} from 'firebase/firestore';
+import {db} from "../firebase/firebaseConfig";
+import {useNavigate} from 'react-router-dom';
 
 export default function CreateLinkPage() {
+  const navigate = useNavigate()
+  const onFinish = async (values) => {
+    const data = {
+      link : values.link,
+      linkq : "1",
+      user_id : "GMhcMVr7PRbHDFKHMXOjVUni40K3"
+  }
+
+    try {
+      const docRef = await addDoc(collection(db, "links"), data);
+
+      navigate("/links")
+      
+      notification.open({
+        message: 'Mensaje',
+        description:
+          'Se ha agregado un gasto',
+        duration: 2,
+      });
+
+      } catch (e) {
+      console.error("Error adding document: ", e);
+      }
+
+  };
+
+  const onFinishFailed = (errorInfo) => {
+     console.log('Failed:', errorInfo);
+  };
+
+  
   return (
     <DashboardLayout>
-        <div>CreateLinkPage</div>
+    <Form
+      name="basic"
+      labelCol={{
+        span: 4,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="link"
+        name="link"
+        rules={[
+          {
+            required: true,
+            message: 'Entre el link',
+          },
+        ]}
+      >
+      <Input />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+           Guardar
+        </Button>
+      </Form.Item>
+    </Form>
     </DashboardLayout>
   )
 }
--- src/pages/LinksPage.js
+++ src/pages/LinksPage.js
@@ -1,79 +1,90 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import DashboardLayout from "./../layout/DashboardLayout";
-import { Space, Table, Tag, Button, Popconfirm } from 'antd';
-import type { ColumnsType } from 'antd/es/table';  
+import { Space, Table, Button, Popconfirm, notification } from 'antd';  
 import { DeleteOutlined } from '@ant-design/icons';
+import {db} from "../firebase/firebaseConfig";
+import {collection, onSnapshot, deleteDoc, doc, query, orderBy, where} from 'firebase/firestore';
 
-interface DataType {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
-}
-
-const handleDelete = (item) => {
-
-}
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: 'Link',
-    dataIndex: 'link',
-    key: 'link',
-    render: text => <a href="{text}">{text}</a>,
-  },
-  {
-    title: 'Link Cortado',
-    dataIndex: 'shortened_link',
-    key: 'agshortened_link',
-    render: text => <a href="{text}">{text}</a>,
-  },
-  {
-    title: 'Acciones',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-          <Popconfirm title="Seguro que quieres borrarlo ?" onConfirm={() => handleDelete(record.key)}>
-              <Button type="primary" icon={<DeleteOutlined />} danger>Eliminar</Button>
-            </Popconfirm>
-      </Space>
-    ),
-  },
-];
-
-const data: DataType[] = [
-  {
-    key: '1',
-    link: 'John Brown',
-    shortened_link: 'aaa',
-  },
-  {
-    key: '2',
-    link: 'https://stackoverflow.com/questions/54387120/cant-wrap-a-menu-item-with-link',
-    shortened_link: '12121212',
-  },
-  {
-    key: '2',
-    link: 'John Brown 2',
-    shortened_link: ' 12',
-  },
-  {
-    key: '2',
-    link: 'John Brown 2',
-    shortened_link: ' 12',
-  },
-  {
-    key: '2',
-    link: 'John Brown 2',
-    shortened_link: ' 12',
-  },
-];
 
 export default function LinksPages() {
-  return (
+  const [links, setLinks] = useState([])
+  const id = "GMhcMVr7PRbHDFKHMXOjVUni40K3"
+
+  const columns  = [
+    {
+      title: 'Link',
+      dataIndex: 'link',
+      key: 'link',
+      render: text => <a href="{text}">{text}</a>,
+    },
+    {
+      title: 'Link Cortado',
+      dataIndex: 'linkq',
+      key: 'linkq',
+      render: text => <a href="{text}">{text}</a>,
+    },
+    {
+      title: 'Acciones',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+            <Popconfirm title="Seguro que quieres borrarlo ?" onConfirm={() => deleteItem(record.id)}>
+                <Button type="primary" icon={<DeleteOutlined />} danger>Eliminar</Button>
+              </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
+  const myquery = query(
+    collection(db, 'links'),
+    where('user_id', '==', id),
+  );
+
+  useEffect(() => {
+    onSnapshot(myquery,
+        (snapshot) => {
+            const data = snapshot.docs.map((documento) => {
+                return {...documento.data(), id: documento.id}
+            })
+            setLinks(data);
+        },
+        (error) => {
+            console.log(error);
+        }
+    );
+}, []);
+
+console.log(links)
+
+
+const deleteItem = async(id) => {
+  console.log(id)
+  try {
+      await deleteDoc(doc(db, 'links', id));
+  } catch(error){
+      console.log('Hubo un error al intentar eliminar el usuario')
+      console.log(error);
+  }
+}
+
+
+return (
     <DashboardLayout>
-         <Table columns={columns} dataSource={data} />
+      <div>
+      <Button
+        href='/create-link'
+        type="primary"
+        style={{
+          marginBottom: 16,
+        }}
+      >
+        Addicionar Link
+      </Button>
+
+      <Table columns={columns} dataSource={links} />
+      </div>
+         
     </DashboardLayout >
   )
 }
